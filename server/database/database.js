90f0105960ff82379c7d6cca266c474a69fd87a8
hien.nguyenhophuoc@hcmut.edu.vn <hien.nguyenhophuoc@hcmut.edu.vn>
2024-12-22 12:46:24 +0700

    Fixx DB

--- server/database/database.js
+++ server/database/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "123456",
   database: "university_management",
 });
 
