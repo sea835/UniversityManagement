0c30fb3b9de433ccbaa68135a246f5a5d341c3ee
ITAHIEU <thomashieunguyen123@gmail.com>
2024-12-24 20:01:54 +0700

    change

--- server/database/database.js
+++ server/database/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'CyberVN*2004',
+    password: '123456',
     database: 'university_management'
 });
 
